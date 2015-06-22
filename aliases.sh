987cd256387e5fabdca923024fd71df1ac992edd
evalle <shmarnev@gmail.com>
2015-06-22 10:21:10 +0200

    add aliases.sh script with some useful aliases

---
+++ aliases.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "alias tmux='tmux -2'" >> ~/.bashrc
+echo "Download shellcheck first!" >> ~/.bashrc
+echo "alias shellcheck='/home/evgeny/.cabal/bin/shellcheck'" >> ~/.bashrc
+echo "also you need to copy github-create function from https://github.com/Evalle/github-create-cli" >> ~/.bashrc
