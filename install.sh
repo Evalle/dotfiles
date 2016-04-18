563474c45f00031098ca8c5a08fc8982e355b2c0
evalle <shmarnev@gmail.com>
2016-04-18 16:54:18 +0200

    Add script for automatical installation of all dotfiles

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# this script will help you to install usful dotfiles
+cp .bashrc $HOME/
+cp .dir_colors $HOME/
+cp -r .git $HOME/
+cp .gitconfig $HOME/
+cp i3/i3/config $HOME/.config/i3/
+cp i3/i3status/config $HOME/.config/i3status/
+cp Monokai.colorscheme $HOME/.local/share/konsole/
+cp .tmux.conf $HOME/
+cp -r .vim $HOME/
+cp .vimrc $HOME/
